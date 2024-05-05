647eaef274401f7a6cc139410da6c105d7901f95
Nesh Kumar <130772656+nash268@users.noreply.github.com>
2024-05-05 18:45:32 +0500

    version 1.0

--- search_copy.bat
+++ search_copy.bat
@@ -13,20 +13,19 @@ if not exist "%destination%" mkdir "%destination%"
 REM Get the drive letter of the USB drive where the script is located
 for %%P in ("%~dp0") do set "usb_drive=%%~dP"
 
-REM Debug output: Display USB drive letter
+REM Display USB drive letter
 echo USB drive letter: %usb_drive%
 
 REM Get list of all drive letters
 for /f %%D in ('wmic logicaldisk get caption ^| findstr /r "[A-Z]:"') do (
     REM Check if the current drive is not the USB drive
     if /i not "%%D"=="%usb_drive%" if /i not "%%D"=="C:" (
-        REM Debug output: Display current drive being searched
+        REM Display current drive being searched
         echo copying from %%D
         REM Search for files with specified extensions on the drive and copy them to the destination folder
         for %%E in (%extensions%) do (
             REM Search for files recursively and copy them to the destination folder
             xcopy "%%D\*%%~nxE" "%destination%" /s /i /y /h
-            REM Debug output: Display number of files copied
         )
     )
 )
