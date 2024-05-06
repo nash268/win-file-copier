c6a2ee9af83ee2be3c88390d4ef8b009c355cfb1
Nesh Kumar <130772656+nash268@users.noreply.github.com>
2024-05-06 21:44:02 +0500

    version 1.0

--- search_copy.bat
+++ search_copy.bat
@@ -1,30 +1,20 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Specify the file extensions to search for
 set "extensions=.pdf .pptx .ppt .docx .doc"
 
-REM Specify the destination folder on the USB drive
 set "destination=%~dp0Files"
 
-REM Create the destination folder if it doesn't exist
 if not exist "%destination%" mkdir "%destination%"
 
-REM Get the drive letter of the USB drive where the script is located
 for %%P in ("%~d0") do set "usb_drive=%%~P"
 
-REM Display USB drive letter
 echo USB drive letter: %usb_drive%
 
-REM Get list of all drive letters
 for /f %%D in ('wmic logicaldisk get caption ^| findstr /r "[A-Z]:"') do (
-    REM Check if the current drive is not the USB drive
     if /i not "%%D"=="%usb_drive%" if /i not "%%D"=="C:" (
-        REM Display current drive being searched
         echo copying from %%D
-        REM Search for files with specified extensions on the drive and copy them to the destination folder
         for %%E in (%extensions%) do (
-            REM Search for files recursively and copy them to the destination folder
             xcopy "%%D\*%%~nxE" "%destination%" /s /i /y /d 2>nul
         )
     )
